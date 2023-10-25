fd274b5c556f03fd29f4033c9bf9cfa8aed6fd7c
GeePaw Hill <geepawhill@geepawhill.org>
2023-10-25 17:31:59 -0400

    Tested strict scoring.

---
+++ core/src/main/kotlin/org/geepawhill/wordle/Game.kt
@@ -0,0 +1,45 @@
+package org.geepawhill.wordle
+
+typealias Eny = String
+
+class Game {
+
+    companion object {
+        fun scoreNaive(answer: String, guess: String): Eny {
+            val upper = guess.uppercase()
+            val indexed = upper.withIndex()
+            var result = Eny()
+            indexed.forEach { item ->
+                if (upper[item.index] == answer[item.index]) result += 'E'
+                else if (answer.contains(upper[item.index])) result += 'Y'
+                else result += 'N'
+            }
+            return result
+        }
+
+        fun scoreStrict(answer: String, guess: String): Eny {
+            val upper = guess.uppercase()
+            val indexed = upper.withIndex()
+            val result = mutableListOf('N','N','N','N','N')
+            val pool = answer.toMutableList()
+            indexed.forEach { item ->
+                if (upper[item.index] == answer[item.index]) {
+                    result[item.index] = 'E'
+                    pool.remove(upper[item.index])
+                }
+            }
+            indexed.forEach { item ->
+                if(result[item.index]!='E') {
+                    if(pool.contains(upper[item.index])) {
+                        pool.remove(upper[item.index])
+                        result[item.index]='Y'
+                    }
+                    else {
+                        result[item.index]='N'
+                    }
+                }
+            }
+            return result.joinToString("")
+        }
+    }
+}
---
+++ core/src/test/kotlin/org/geepawhill/wordle/GameTest.kt
@@ -0,0 +1,17 @@
+package org.geepawhill.wordle
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class GameTest {
+
+
+    @Test
+    fun `scores strictly`() {
+        assertThat(Game.scoreStrict("AAAAA","AAAAA")).isEqualTo("EEEEE")
+        assertThat(Game.scoreStrict("AAAAA","BBBBB")).isEqualTo("NNNNN")
+        assertThat(Game.scoreStrict("ABCDE","ABABA")).isEqualTo("EENNN")
+        assertThat(Game.scoreStrict("EMAIL","ELOPE")).isEqualTo("EYNNN")
+        assertThat(Game.scoreStrict("CREDO","CREED")).isEqualTo("EEENY")
+    }
+}
