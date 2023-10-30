636b8616d1cac0b8c2e9a00acb8a11004d6a8f41
GeePaw Hill <geepawhill@geepawhill.org>
2023-10-30 15:48:51 -0400

    Improved UI, not.

--- gui/src/main/kotlin/org/geepawhill/wordle/FirstSolverModel.kt
+++ gui/src/main/kotlin/org/geepawhill/wordle/FirstSolverModel.kt
@@ -1,16 +1,29 @@
 package org.geepawhill.wordle
 
+import javafx.beans.property.SimpleBooleanProperty
 import javafx.beans.property.SimpleStringProperty
 import tornadofx.*
 
-class Problem(val answer: String, val guesses: List<String>) {
+class Problem(val answer: String, guesses: List<String>) {
     val size = guesses.size
     val guessesString = guesses.joinToString(" ")
     val win = guesses.size < 7
 }
 
+class Batch(val id: String) {
+    val problems = observableListOf<Problem>()
+    var wins = 0
+    var totalGuesses = 0
+    fun add(problem: Problem) {
+        if (problem.win) wins += 1
+        totalGuesses += problem.size
+    }
+}
+
 class FirstSolverModel(val dataset: Dataset) : Reporter {
 
+    val needsPreparing = SimpleBooleanProperty(true)
+    val batches = observableListOf<Batch>()
     val problems = observableListOf<Problem>()
 
     val firstGuess = SimpleStringProperty("VALOR")
@@ -18,17 +31,29 @@ class FirstSolverModel(val dataset: Dataset) : Reporter {
 
     val guesses = mutableListOf<String>()
 
+    var batchInProgress = Batch("N/A")
+
+    fun prepare() {
+        solver.prepare()
+        needsPreparing.set(false)
+    }
+
     fun run2315() {
         val runner = Runner(this)
+        solver.first = firstGuess.value
         task {
             runner.run(solver, dataset)
         }
     }
 
     override fun startBatch(id: String) {
+        batchInProgress = Batch(id)
     }
 
     override fun endBatch() {
+        runLater {
+            batches.add(batchInProgress)
+        }
     }
 
     override fun startRun(id: String) {
@@ -41,8 +66,13 @@ class FirstSolverModel(val dataset: Dataset) : Reporter {
 
     override fun endRun(answer: String) {
         val problem = Problem(answer, guesses)
-        runLater {
-            problems.add(problem)
+        batchInProgress.add(problem)
+    }
+
+    fun runSolutions() {
+        for (solution in dataset.solutions) {
+            firstGuess.value = solution
+            run2315()
         }
     }
 }
--- gui/src/main/kotlin/org/geepawhill/wordle/FirstSolverView.kt
+++ gui/src/main/kotlin/org/geepawhill/wordle/FirstSolverView.kt
@@ -4,36 +4,64 @@ import javafx.scene.Parent
 import tornadofx.*
 
 class FirstSolverView(model: FirstSolverModel) : Fragment() {
+
     override val root: Parent = borderpane {
         top = toolbar {
+            button("Prepare") {
+                font = Styles.textFont
+                enableWhen(model.needsPreparing)
+                action {
+                    model.prepare()
+                }
+            }
+            textfield(model.firstGuess) {
+                font = Styles.textFont
+            }
             button("Run 2315") {
+                font = Styles.textFont
                 action {
                     model.run2315()
                 }
             }
-        }
-        center = tableview(model.problems) {
-            columnResizePolicy = SmartResize.POLICY
-            readonlyColumn("W/L", Problem::win).cellFormat {
-                if (it == true) {
-                    text = "W"
-                    background = Styles.winBackground
-                } else {
-                    text = "L"
-                    background = Styles.lossBackground
+
+            button("Run Solutions") {
+                font = Styles.textFont
+                action {
+                    model.runSolutions()
                 }
             }
-            readonlyColumn("Answer", Problem::answer).cellFormat {
-                text = it
-                font = Styles.textFont
-            }
-            readonlyColumn("Size", Problem::size).cellFormat {
-                text = it.toString()
-                font = Styles.textFont
-            }
-            readonlyColumn("Guesses", Problem::guessesString).cellFormat {
-                text = it
-                font = Styles.textFont
+        }
+        center = tableview(model.batches) {
+            columnResizePolicy = SmartResize.POLICY
+            readonlyColumn("ID", Batch::id)
+            readonlyColumn("W", Batch::wins)
+            readonlyColumn("Average", Batch::totalGuesses)
+            rowExpander(expandOnDoubleClick = true) {
+                paddingLeft = expanderColumn.width
+                tableview(it.problems) {
+                    columnResizePolicy = SmartResize.POLICY
+                    readonlyColumn("W/L", Problem::win).cellFormat {
+                        if (it == true) {
+                            text = "W"
+                            background = Styles.winBackground
+                        } else {
+                            text = "L"
+                            background = Styles.lossBackground
+                        }
+                    }
+                    readonlyColumn("Answer", Problem::answer).cellFormat {
+                        text = it
+                        font = Styles.textFont
+                    }
+                    readonlyColumn("Size", Problem::size).cellFormat {
+                        text = it.toString()
+                        font = Styles.textFont
+                    }
+                    readonlyColumn("Guesses", Problem::guessesString).cellFormat {
+                        text = it
+                        font = Styles.textFont
+                    }
+                }
             }
         }
     }
